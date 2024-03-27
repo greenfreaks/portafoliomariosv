273e1369d3047c900628af8ac145efc94f6d00b7
Mario Sandoval Velázquez <mariosandovalv1998@gmail.com>
2024-03-27 14:35:30 -0600

    Se crea estructura de los componentes del sitio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MenuComponent } from './menu/menu.component';
+import { BodyComponent } from './body/body.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { CertificacionesComponent } from './certificaciones/certificaciones.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    MenuComponent,
+    BodyComponent,
+    InicioComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    CertificacionesComponent
   ],
   imports: [
     BrowserModule,
